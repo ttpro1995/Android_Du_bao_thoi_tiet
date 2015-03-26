6fb5d02779ba6ea55684a02d493c2b2a03f64fdc
ThaiThien <tthien@apcs.vn>
2015-03-26 21:53:14 +0700

    init

---
+++ app/src/main/java/com/hahattpro/dubaothoitiet/ForecastFragment.java
@@ -0,0 +1,25 @@
+package com.hahattpro.dubaothoitiet;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by haha on 3/26/2015.
+ */
+public class ForecastFragment extends Fragment {
+    public ForecastFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+
+
+}
+
